e07a8ed7c2230381bb6028321cb07b48a9c93985
EmmyXambeg <xamsinnovation@gmail.com>
2023-09-13 06:01:29 +0100

    adding pycodestyle

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -8,7 +8,7 @@ del l[1]
 lib.print_python_list_info(l)
 l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
 lib.print_python_list_info(l)
-l =[]
+l = []
 lib.print_python_list_info(l)
 l.append(0)
 lib.print_python_list_info(l)
